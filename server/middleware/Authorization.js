dede8ebe757e9db74d132b0df26a1bc5b316b905
Zach <dzacharymason@gmail.com>
2021-07-02 21:40:20 -0500

    Check whether jwt token is valid

--- server/middleware/Authorization.js
+++ server/middleware/Authorization.js
@@ -3,13 +3,15 @@ require('dotenv').config()
 
 module.exports = async (req, res, next) => {
     try {
-        const jwToken = req.header('token')
+        const jwtToken = req.header('token')
 
         if (!jwtToke) {
             return res.status(403).json('Not Authorized')
         }
 
-        const payload = jwt.verify
+        const payload = jwt.verify(jwtToken, process.env.jwtSecret) //chekcs if the jwt Token is valid
+
+        req.user = payload.userjj
     } catch (err) {
         console.error(err.message)
         res.status(403).json('Not Authorized')
