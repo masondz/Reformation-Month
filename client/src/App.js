a5975a9433b527cff8e6a9b5d8da4f52787cb2e1
Zach <dzacharymason@gmail.com>
2021-07-04 10:45:40 -0500

    Set-up client side of app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+function App() {
+  return <Fragment>Hi planet</Fragment>;
+}
+
+export default App;
