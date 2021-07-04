478761512b6f401c97e379521725b484ef596710
Zach <dzacharymason@gmail.com>
2021-07-04 14:30:56 -0500

    Add local storage for token

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,75 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
-const Register = () => {
+const Register = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    user_password: "",
+    first_name: "",
+    last_name: "",
+  });
+  const { email, user_password, first_name, last_name } = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmiForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { email, user_password, first_name, last_name };
+      const response = await fetch("http://localhost:5000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+
+        body: JSON.stringify(body),
+      });
+      const parseRes = await response.json();
+
+      localStorage.setItem("token", parseRes.token);
+      setAuth(true);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  //Building the form. Form inputs must be consisistant with req.body of the serve i.e. the name of the inputs must the be the same as the keys for the request body
   return (
     <Fragment>
-      <h1>Register</h1>
+      <h1 className="text-center my-5">Register</h1>
+      <form onSubmit={onSubmiForm}>
+        <input
+          className="form-control my-3"
+          type="text"
+          name="first_name"
+          placeholder="First Name"
+          value={first_name}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          className="form-control my-3"
+          type="text"
+          name="last_name"
+          placeholder="Last Name"
+          value={last_name}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="form-control my-3"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          className="form-control my-3"
+          type="password"
+          name="user_password"
+          placeholder="Password"
+          value={user_password}
+          onChange={(e) => onChange(e)}
+        />
+        <button className="btn btn-success btn-block">Submit</button>
+      </form>
     </Fragment>
   );
 };
