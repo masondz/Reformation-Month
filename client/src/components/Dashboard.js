8ce087b05351d9fbe884d455bc1eeaf52f804994
Zach <dzacharymason@gmail.com>
2021-07-04 23:18:50 -0500

    Add GET and logout functionality

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 const Dashboard = ({ setAuth }) => {
+  const [firstName, setFirstName] = useState("");
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      setFirstName(parseRes.first_name);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
+
+  useEffect(() => {
+    getName();
+  }, []);
+
   return (
     <Fragment>
-      <h1>Dashboard</h1>
-      <button onClick={() => setAuth(false)}>Logout</button>
+      <h1>Dashboard {firstName}</h1>
+      <button className="btn btn-primary" onClick={(e) => logout(e)}>
+        Logout
+      </button>
     </Fragment>
   );
 };
