599c992abb3f4862ae0b59977378f5c68ac534b1
Zach <dzacharymason@gmail.com>
2021-07-07 01:13:19 -0500

    Add state, onChange for value, more option data

--- client/src/components/FindChallenge.js
+++ client/src/components/FindChallenge.js
@@ -2,6 +2,13 @@ import React, { Fragment, useState, useEffect } from "react";
 
 const FindChallenge = () => {
   const [challengeList, setChallengeList] = useState([]);
+  const [inputs, setInputs] = useState({
+    challenge_name: "find your challenge",
+    id: "",
+    organization: "",
+  });
+
+  const { challenge_name, id, organization } = inputs;
 
   async function getChallenges() {
     try {
@@ -25,20 +32,38 @@ const FindChallenge = () => {
     getChallenges();
   }, []);
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, challenge_name: e.target.value });
+  };
+
+  console.log(challenge_name);
   return (
     <Fragment>
-      <label for="chal-list">Find a reading Challenge</label>
-      <input
-        className="form-control"
-        list="challenge-list"
-        name="chal-list"
-        id="chal-list"
-      />
-      <datalist id="challenge-list">
-        {challengeList.map((challenge) => {
-          return <option value={challenge.challenge_name} />;
-        })}
-      </datalist>
+      <form onSubmit={onSubmit}>
+        <label for="chal-list">Find a reading Challenge</label>
+        <input
+          value={challenge_name}
+          onChange={(e) => onChange(e)}
+          className="form-control"
+          list="challenge-list"
+          name="chal-list"
+          id="chal-list"
+        />
+        <datalist id="challenge-list">
+          {challengeList.map((challenge) => {
+            return (
+              <option value={challenge.challenge_name}>
+                Read {challenge.goal} Chapters
+              </option>
+            );
+          })}
+        </datalist>
+      </form>
     </Fragment>
   );
 };
