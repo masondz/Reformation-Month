7e5e4e36af4c581f216a99f8f021e1d7e23f8ab9
Zach <dzacharymason@gmail.com>
2021-07-09 15:24:28 -0500

    Add update challenge query

--- server/routes/challengeDashboard.js
+++ server/routes/challengeDashboard.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const pool = require('../db')
 const authorization = require('../middleware/Authorization')
 
+//create challenge
 router.post('/', authorization, async (req, res) => {
     try {
         const {
@@ -40,6 +41,60 @@ router.post('/', authorization, async (req, res) => {
             ]
         )
         res.json(challengeToAdd.rows[0])
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//get single challenges
+router.get('/:challenge_name', authorization, async (req, res) => {
+    try {
+        const challengeName = req.params.challenge_name
+        const challenge = await pool.query(
+            'SELECT * FROM reading_challenges WHERE challenge_name=$1',
+            [challengeName]
+        )
+        res.json(challenge.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//update challenge
+router.put('/', authorization, async (req, res) => {
+    try {
+        const {
+            challenge_name,
+            organization,
+            challenge_type,
+            challenge_admin,
+            goal,
+            id,
+        } = req.body
+        //check if challenge exists
+        const challengeExist = await pool.query(
+            `SELECT challenge_name FROM reading_challenges WHERE challenge_name = $1`,
+            [challenge_name]
+        )
+        if ((challengeExist.rowCount = 0)) {
+            return res
+                .status(401)
+                .json(`Reading challenge ${challenge_name} does not exist.`)
+        }
+        //check if authorized to change challenge
+
+        //udate challenge
+        const updateChallenge = await pool.query(
+            //cannont change admin
+            `UPDATE reading_challenges 
+            SET challenge_name = $1,
+                organization = $2,
+                challenge = $3,
+                goal = $4
+            WHERE id = $5 RETURNING *`,
+            [challenge_name, organization, challenge_type, goal, id]
+        )
+        res.json(updateChallenge)
     } catch (err) {
         console.error(err.message)
     }
