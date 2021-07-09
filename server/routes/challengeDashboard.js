763adbb9bbbde2c44864fe9c08501af14b03ad7b
Zach <dzacharymason@gmail.com>
2021-07-09 17:06:42 -0500

    REALLY add reader authorization check for challenge

--- server/routes/challengeDashboard.js
+++ server/routes/challengeDashboard.js
@@ -64,25 +64,32 @@ router.get('/:challenge_name', authorization, async (req, res) => {
 router.put('/', authorization, async (req, res) => {
     try {
         const {
+            reader_id,
             challenge_name,
             organization,
             challenge_type,
-            challenge_admin,
             goal,
             id,
         } = req.body
         //check if challenge exists
         const challengeExist = await pool.query(
-            `SELECT challenge_name FROM reading_challenges WHERE challenge_name = $1`,
+            `SELECT challenge_name, challenge_admin FROM reading_challenges WHERE challenge_name = $1`,
             [challenge_name]
         )
-        if ((challengeExist.rowCount = 0)) {
+        console.log(challengeExist)
+        if (challengeExist.rowCount === 0) {
             return res
                 .status(401)
                 .json(`Reading challenge ${challenge_name} does not exist.`)
+        } else if (challengeExist.rows[0].challenge_admin !== reader_id) {
+            return res
+                .status(403)
+                .json(
+                    `You are not authorized to update challenge ${challenge_name}`
+                )
         }
-        //check if authorized to change challenge
 
+        //check if authorized to change challenge
         //udate challenge
         const updateChallenge = await pool.query(
             //cannont change admin
@@ -94,7 +101,7 @@ router.put('/', authorization, async (req, res) => {
             WHERE id = $5 RETURNING *`,
             [challenge_name, organization, challenge_type, goal, id]
         )
-        res.json(updateChallenge)
+        res.json(updateChallenge.rows[0])
     } catch (err) {
         console.error(err.message)
     }
