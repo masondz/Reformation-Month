71178072716de1beca57d2fe1af197468c155565
Zach <dzacharymason@gmail.com>
2021-07-09 01:31:50 -0500

    Add post route to challengeDashboard.js

--- server/routes/challengeDashboard.js
+++ server/routes/challengeDashboard.js
@@ -2,4 +2,36 @@ const router = require('express').Router()
 const pool = require('../db')
 const authorization = require('../middleware/Authorization')
 
+router.post('/', async (req, res) => {
+    const {
+        challenge_name,
+        organization,
+        challenge_type,
+        challenge_admin,
+        goal,
+    } = req.body
+    try {
+        const challengeToAdd = await pool.query(
+            `INSERT INTO reading_challenges (
+                challenge_name, 
+                organization, 
+                challenge, 
+                challenge_admin,
+                goal)
+                
+                VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+            [
+                challenge_name,
+                organization,
+                challenge_type,
+                challenge_admin,
+                goal,
+            ]
+        )
+        res.json(challengeToAdd.rows[0])
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
 module.exports = router
