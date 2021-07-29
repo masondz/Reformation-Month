7b6ec6cf85da25501ef671a622fa788d773b6f9e
masondz <dzacharymason@gmail.com>
2021-07-29 08:13:10 -0500

    Hide

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "AmorDei@2011",
+    password: "AXXXXXXXXXX",
     host: "localhost",
     database: "reformation_month"
     }
