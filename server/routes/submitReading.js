0c1f04a200162bc2ad609524c83f6cd30bfc7e8e
Zach <dzacharymason@gmail.com>
2021-08-27 07:01:10 -0500

    add additional_reader rout to submitReading

--- server/routes/submitReading.js
+++ server/routes/submitReading.js
@@ -3,7 +3,7 @@ const pool = require('../db')
 const authorization = require('../middleware/Authorization')
 const validInputs = require('../middleware/validinputs')
 
-//add new totals
+//add new totals for readers
 
 router.put('/', authorization, async (req, res) => {
     try {
@@ -36,6 +36,37 @@ router.put('/', authorization, async (req, res) => {
     }
 })
 
-//
+//add new total for additional_readers
+
+router.put('/additional-readers', authorization, async (req, res) => {
+    try {
+        const { challenge_type, total, ad_reader_id } = req.body
+        console.log(req.body)
+        if (challenge_type === 'books_read') {
+            console.log('Update books read')
+            const updateTotal = await pool.query(
+                `UPDATE additional_readers SET books_read = books_read + $1 WHERE ad_reader_id = $2 RETURNING *`,
+                [total, ad_reader_id]
+            )
+            res.json(updateTotal.rows[0].books_read)
+        } else if (challenge_type === 'chapters_read') {
+            console.log('Update chapters read')
+            const updateTotal = await pool.query(
+                `UPDATE additional_readers SET chapters_read = chapters_read + $1 WHERE ad_reader_id = $2 RETURNING *`,
+                [total, ad_reader_id]
+            )
+            res.json(updateTotal.rows[0].chapters_read)
+        } else {
+            console.log('Update verses memorized')
+            const updateTotal = await pool.query(
+                `UPDATE additional_readers SET verses_memorized = verses_memorized + $1 WHERE ad_reader_id = $2 RETURNING *`,
+                [total, ad_reader_id]
+            )
+            res.json(updateTotal.rows[0].verses_memorized)
+        }
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 module.exports = router
