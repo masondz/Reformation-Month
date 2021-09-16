c573b75a48c780a790e35e88f7b30c8ca1490478
masondz <dzacharymason@gmail.com>
2021-09-16 05:09:10 -0500

    Update ChallengeTotal.js

--- client/src/components/ChallengeTotal.js
+++ client/src/components/ChallengeTotal.js
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from "react";
 
 const ChallengeTotal = ({ setAuth, challenge }) => {
-  
+  const challenge_id = challeng.id
   const [total, setTotal] = useState("...");
   
   const getTotals = () => {
-    return total;
+    try{
+      const;
+      const getTotal = await 
+      
+    }catch(err){
+      console.error(err.message)
+    }
   }
   
   return <>{challenge.challenge_name} total</>;
