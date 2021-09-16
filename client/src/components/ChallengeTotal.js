20f7fab32a50ae06af1990f3abfabb0881e56d00
masondz <dzacharymason@gmail.com>
2021-09-16 00:46:26 -0500

    Add state for totals

--- client/src/components/ChallengeTotal.js
+++ client/src/components/ChallengeTotal.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const ChallengeTotal = ({ setAuth, challenge }) => {
+  
+  const [total, setTotal] = useState("...");
+  
+  const getTotals = () => {
+    return total;
+  }
+  
   return <>{challenge.challenge_name} total</>;
 };
 
