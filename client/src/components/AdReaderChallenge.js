7ffe23062769d83cc35ac0049c06d5a329a4239e
masondz <dzacharymason@gmail.com>
2021-10-08 02:25:06 -0500

    work on fetch for challenges

--- client/src/components/AdReaderChallenge.js
+++ client/src/components/AdReaderChallenge.js
@@ -1,6 +1,21 @@
 import React, {useState, useEffect} from 'react';
 
 const AdReaderChallenges = ({setAuth, adReader}) => {
+  const adReaderId = adReader.ad_reader_id;
+  
+  useEffect(() => {
+    const adReaderId = adReader.ad_reader_id;
+    const getChallenges = async (adReaderId) => {
+      try {
+        const challenges = await fetch('/additional-readers/${adReaderId}', (req, res) => {
+          
+        })
+      } catch (err){
+        console.error(err.message);
+      }
+    }
+  })
+  
   return <p><i>Modify member's challenges coming soon!</i></p>
 }
 
