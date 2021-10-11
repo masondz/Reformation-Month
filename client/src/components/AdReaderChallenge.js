98ba44c5d7d7f4a864cdf2c0aa85a095830443b1
masondz <dzacharymason@gmail.com>
2021-10-11 03:28:49 -0500

    Update AdReaderChallenge.js

--- client/src/components/AdReaderChallenge.js
+++ client/src/components/AdReaderChallenge.js
@@ -31,8 +31,7 @@ const AdReaderChallenges = ({setAuth, adReader}) => {
   
   return( 
     <div>
-    <p><i>Modify member's challenge coming soon!</i></p>
-    {challenges.map(challenge => {console.log(Object.keys(challenge))}} 
+     <p><i>Modify member's challenge coming soon!</i></p>
     </div>
   )
 }
