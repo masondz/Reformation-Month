9731ea7122e22c1dc3574a44a203ec7ba7d82319
masondz <dzacharymason@gmail.com>
2021-10-14 03:14:29 -0500

    Try bootstrap modal to leave family group

--- client/src/components/CreateAdditionalReader.js
+++ client/src/components/CreateAdditionalReader.js
@@ -103,12 +103,15 @@ const CreateAdditionalReader = ({
         return console.log('you submitted something')
     }
 
+    
+    
+ 
     return (
         <div>
             <div className="d-grid gap-2 col-6 mx-auto">
                 <button
                     type="button"
-                    class="btn btn-outline-primary btn-sm"
+                    className="btn btn-outline-primary btn-sm"
                     data-bs-toggle="modal"
                     data-bs-target="#make-additional-reader"
                 >
@@ -118,13 +121,15 @@ const CreateAdditionalReader = ({
                     button
                     type="button"
                     className="btn btn-outline-danger btn-sm"
-                    onClick={() => leaveFG()}
+                    data-bs-toggle="modal"
+                    data-bs-target="#leave-fg"
+                    
                 >
                     Leave Family Group
                 </button>
             </div>
 
-            {/* // <!-- Modal --> */}
+            {/* // <!-- Modal FOR ADDING MEMBERS --> */}
             <div
                 class="modal fade"
                 id="make-additional-reader"
@@ -170,10 +175,10 @@ const CreateAdditionalReader = ({
                                 </lable>
                             </form>
                         </div>
-                        <div class="modal-footer">
+                        <div className="modal-footer">
                             <button
                                 type="button"
-                                class="btn btn-secondary"
+                                className="btn btn-secondary"
                                 data-bs-dismiss="modal"
                                 onClick={() => resetInput()}
                             >
@@ -182,7 +187,7 @@ const CreateAdditionalReader = ({
                             <button
                                 type="submit"
                                 data-dismiss="modal"
-                                class="btn btn-primary"
+                                className="btn btn-primary"
                                 form="make-additional-readerform"
                             >
                                 Submit
@@ -191,6 +196,52 @@ const CreateAdditionalReader = ({
                     </div>
                 </div>
             </div>
+
+
+           {/* // <!-- Modal FOR leaving family group --> */}
+            <div
+                className="modal fade"
+                id="leave-fg"
+                tabindex="-1"
+                aria-labelledby="leave-fgLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Are you sure you want to leave Family Group?</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                type="button"
+                                data-dismiss="modal"
+                                className="btn btn-warning"
+                                data-bs-dismiss="modal"
+                                onClick={() => leaveFG()}
+                            >
+                                Leave
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     )
 }
