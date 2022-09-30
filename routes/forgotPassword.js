2c4f35765d6bfea2a5df8b6a0a799f515af54abe
Zach <dzacharymason@gmail.com>
2022-09-29 20:08:50 -0500

    hard code somehting

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -34,8 +34,8 @@ if (
             const transporter = nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
-                    user: `${process.env.EMAIL_ADDRESS}`,
-                    pass: `${process.env.EMAIL_PASSWORD}`,
+                    user: 'RMonthHelp@gmail.com',
+                    pass: 'ygctypkoowyrlnyd',
                 },
             })
 
