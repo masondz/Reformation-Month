d1b96e73b798bf349beb933a0c2e1f6ed60087af
Zach <dzacharymason@gmail.com>
2023-01-25 10:04:17 -0600

    go back to strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "./index.css";
-import App from "./App";
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById("root"));
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root')
+)
