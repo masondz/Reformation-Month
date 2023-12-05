b3851cc2252c7e97250a4123070fc460a096d5ef
David Zachary Mason <dzacharymason@gmail.com>
2023-12-05 05:27:38 -0600

    fix string error in togglePeek function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = ({ setAuth }) => {
     }
 
     const togglePeek = () => {
-        inputType === "password" ? setPeekPass("text") : setPeekPass("password);
+        inputType === "password" ? setPeekPass("text") : setPeekPass("password");
     };
 
     const onSubmitForm = async (e) => {
